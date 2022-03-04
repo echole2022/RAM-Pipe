8bd5e97015bd477544d5427b2c6d00f075d4020f
echole2022 <echo.peiyi.huang@gmail.com>
2022-03-04 13:40:49 +0100

    modified

--- app.R
+++ app.R
@@ -2,34 +2,80 @@ library(shiny)
 library(shinysurveys)
 library(readxl)
 
-q1 <- data.frame(question = "What is your favorite food?",
-                 option = "Your Answer",
+q1 <- data.frame(question = "Vorname",
+                 option = "First Name",
                  input_type = "text",
-                 input_id = "favorite_food",
+                 input_id = "first_name",
                  dependence = NA,
                  dependence_value = NA,
-                 required = F)
-q2 <- data.frame(question = "What is your  food?",
-                 option = "Your Answer",
+                 required = T)
+q2 <- data.frame(question = "Nachname",
+                 option = "Last Name",
                  input_type = "text",
-                 input_id = "favorite",
+                 input_id = "last_name",
                  dependence = NA,
                  dependence_value = NA,
-                 required = F)
-df <- rbind(q1, q2)
+                 required = T)
+q3 <- data.frame(question = "Studium/Typ der Arbeit",
+                 option = "Study/Job Type",
+                 input_type = "text",
+                 input_id = "study_job",
+                 dependence = NA,
+                 dependence_value = NA,
+                 required = T)
+q4 <- data.frame(question = "Fachbereich",
+                 option = "Subject",
+                 input_type = "text",
+                 input_id = "subject",
+                 dependence = NA,
+                 dependence_value = NA,
+                 required = T)
+q5 <- data.frame(question = "Erst Betreuer/in",
+                 option = "Main Supervisor",
+                 input_type = "text",
+                 input_id = "main_supervisor",
+                 dependence = NA,
+                 dependence_value = NA,
+                 required = T)
+q6 <- data.frame(question = "Sind Sie RAM-Mitglieder?",
+                 option = c("Ja","Nein"),
+                 input_type = "y/n",
+                 input_id = "membeship",
+                 dependence = NA,
+                 dependence_value = NA,
+                 required = T)
+q7 <- data.frame(question = "Was ist der Preis bzw. Fördungersart, den Sie beantragen möchten?",
+                 option = c("Teilförderung","Otto-Selz Preis","Förderung","RAT-Preis","RAM-Preis"),
+                 input_type = "mc",
+                 input_id = "prize_subsidary_type",
+                 dependence = NA,
+                 dependence_value = NA,
+                 required = T)
+q8 <- data.frame(question = "Bitte beschreiben Sie den Zweck der Förderung:",
+                 option = "Please describe the purpose of this prize or subsidary",
+                 input_type = "text",
+                 input_id = "purpose",
+                 dependence = NA,
+                 dependence_value = NA,
+                 required = T)
+q9 <- data.frame(question = "Preisgeld bzw. Förderungsbetrag (€):",
+                 option = "Amount of prize or subsidary (€)",
+                 input_type = "text",
+                 input_id = "amount",
+                 dependence = NA,
+                 dependence_value = NA,
+                 required = T)
+df <- rbind(q1, q2, q3, q4, q5, q6, q7, q8, q9)
 
 ui <- fluidPage(
     surveyOutput(df = df,
-                 survey_title = "Hello, World!",
-                 survey_description = "Welcome! This is a demo survey showing off the {shinysurveys} package."),
-    htmlOutput("text")
+                 survey_title = "RAM Application",
+                 survey_description = "Welcome! demo")
 )
 
 server <- function(input, output, session) {
     renderSurvey()
-    output$text <- renderText({ 
-      input$favorite 
-    })
+    
     observeEvent(input$submit, {
         showModal(modalDialog(
             title = "Congrats, you completed your first shinysurvey!",
